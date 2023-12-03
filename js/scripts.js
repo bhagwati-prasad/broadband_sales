0e3a9dfc45b5673ad908c93d611bfff37df82ea5
bhagwati-prasad <bhagwati.indoria@gmail.com>
2023-12-03 22:48:36 +0000

    Homepage

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-console.log("hello");
+function setupCarousel(elem) {
+    if(!elem) {
+        return;
+    }
+    
+    let items = document.querySelectorAll(elem);
+    items.forEach((el) => {
+        const minPerSlide = 4
+        let next = el.nextElementSibling
+        for (var i=1; i<minPerSlide; i++) {
+            if (!next) {
+                next = items[0]
+            }
+            let cloneChild = next.cloneNode(true)
+            el.appendChild(cloneChild.children[0])
+            next = next.nextElementSibling
+        }
+    });
+}
+
+setupCarousel(".logoTicker .carousel .carousel-item")
+setupCarousel(".internetProviders .carousel .carousel-item")
